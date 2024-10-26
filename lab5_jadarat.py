3f0c2c9daa2c508048c392367c12d5bfaadfd75d
kawthar-1411 <eknjz3@outlook.sa>
2024-10-26 21:11:35 +0300

    Create lab5_jadarat.py

---
+++ lab5_jadarat.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.markdown("<h1 style='text-align: center;'>تساؤلات كثيرة حول البحث عن عمل</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; direction: rtl;'> مما لا شك فيه وجود الكثير من التساؤلات في أذهان الباحثين والباحثات عن عمل. 'راح نجاوب على كل هذه التساؤلات بناء على تحليل عينة مكونة من 1700 اعلان وظيفي وفي احد أكثر المنصات الحكومية الموثوقة وهي منصة جدارات'</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; direction: rtl;'> </p>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center;'>اولا :سنوات الخبرة</h2>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; direction: rtl;'> الكثير من الخريجين يشعرون بالقلق حيال عدد سنوات الخبرة وانها تؤثر بشكل سلبي على الحصول على الوظيفة ولكن بعد عمل الدراسة تبين ان الاعلانات الوظيفية لحديثي التخرج تفوق الاعلانات الوظيفية لذوي الخبرة واليك رسم بياني يوضح النسب لذلك اوصي حديثي التخرج عدم القلق حيال الخبرة والسعي لتطوير مهاراتهم باستمرار لمواكبة متطلبات سوق العمل </p>", unsafe_allow_html=True)
+st.image("C:/Users/Eknjz/Pictures/Screenshots/لقطة شاشة 2024-10-26 173859.png", caption="تأثير سنوات الخبرة على عدد الوظائف المعلن عنها", use_column_width=True)
+
+
+st.markdown("<h2 style='text-align: center;'>ثانيا:الجنس</h2>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; direction: rtl;'>كما أن كل من الذكور يقولون ان اغلب الوظائف نسائية وكذلك النساء يعتقدون ان اغلب الوظائف رجالية والحقيقة هي أن تقريبا النساء والرجال متساويان في الاعلانات الوظيفية </p>", unsafe_allow_html=True)
+st.image("C:/Users/Eknjz/Pictures/Screenshots/لقطة شاشة 2024-10-26 172747.png", caption="النسب المئوية للاعلانات الوظيفية مرتبطة بالجنس", use_column_width=True)
+
+
+st.markdown("<h2 style='text-align: center;'>ثالثا:المناطق الجغرافية</h2>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right; direction: rtl;'>كما أن أغلب الاعلانات الوظيفية تكون ضمن مناطق جغرافية محددة وذلك عائد الى تنوع الاعمال والنشاطات التجارية في تلك الاماكن واليكم توزيع الاعلانات الوظيفية تبعا للمنطقة الجغرافية</p>", unsafe_allow_html=True)
+st.image("C:/Users/Eknjz/Pictures/Screenshots/لقطة شاشة 2024-10-26 173552.png", caption='توزيع الاعلانات الوظيفية بناء على المنطقة الجغرافية', use_column_width=True)
